2a644f1719e40515c205b0dfbcb05519370f120a
Eric Grimes <erictgrimes@gmail.com>
2025-06-28 10:30:34 -0700

    removed count

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import ContactList from "./components/ContactList";
 import SelectedContact from "./components/SelectedContact";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [selectedContactId, setSelectedContactId] = useState(null);
 
   return (
